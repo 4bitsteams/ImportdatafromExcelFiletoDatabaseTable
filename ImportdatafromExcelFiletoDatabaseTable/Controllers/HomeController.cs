3c47a5baef0436ebf3ff25a81a414ab58345259e
md.rubel <md.rubel@HOAP2406101764A>
2021-09-25 14:37:22 +0600

    Design Import Method

--- ImportdatafromExcelFiletoDatabaseTable/Controllers/HomeController.cs
+++ ImportdatafromExcelFiletoDatabaseTable/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using ImportdatafromExcelFiletoDatabaseTable.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,6 +24,33 @@ namespace ImportdatafromExcelFiletoDatabaseTable.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<List<Subject>> Import(IFormFile formFIle)
+        {
+            var SubjectList = new List<Subject>();
+            using (var stream = new MemoryStream())
+            {
+                await formFIle.CopyToAsync(stream);
+                using (var package = new ExcelPackage(stream))
+                {
+                    ExcelWorksheet excelWorksheet = package.Workbook.Worksheets[0];
+                    var rowCount = excelWorksheet.Dimension.Rows;
+                    for (int row = 0; row < rowCount; row++)
+                    {
+                        SubjectList.Add(new Subject
+                        {
+
+                            RefId = (int)excelWorksheet.Cells[row, 1].Value,
+                            Code = excelWorksheet.Cells[row, 2].Value.ToString().Trim(),
+                            Name = excelWorksheet.Cells[row, 3].Value.ToString().Trim(),
+                            Description = excelWorksheet.Cells[row, 3].Value.ToString().Trim(),
+                        });
+                    }
+                }
+            }
+
+            return SubjectList;
         }
 
         public IActionResult Privacy()
