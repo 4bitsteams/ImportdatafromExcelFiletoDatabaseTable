bdb16a13ddbaf87db9def0f8ca78abbe50cb13ae
md.rubel <md.rubel@HOAP2406101764A>
2021-09-25 13:28:43 +0600

    Add project files.

---
+++ ImportdatafromExcelFiletoDatabaseTable/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ImportdatafromExcelFiletoDatabaseTable.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
